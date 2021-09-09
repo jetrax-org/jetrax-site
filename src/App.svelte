2b8516727dc8b59a08119e3405343819f57952e4
breadA <dhairysrivastava5@gmail.com>
2021-09-09 15:20:58 +0530

    basic template

---
+++ src/App.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import { fade } from "svelte/transition";
+  import { draw } from "svelte/transition";
+  import Toggler from "./ViewMode.svelte";
+</script>
+
+<title>Jetrax</title>
+
+<div>
+  <meta name="viewport" content="width=device-width,initial-scale=1" />
+  <script
+    src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.0.4/socket.io.js"></script>
+  <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+  />
+  <Toggler>Toggle</Toggler>
+  <main>
+    <h1>Jetrax</h1>
+    <p>A next gen chat platform for developers</p>
+    <button>Click here to learn more</button>
+  </main>
+  <div >
+   <!-- <img src="./pic.png">
+   <p class="imagecol">sadasdasd</p> -->
+  </div>
+</div>  
+
+<style lang="scss">
+  @import "./styles/global.scss";
+  @import "./styles/_darkMode.scss";
+</style>
---
+++ src/ChatMessages.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { fade } from "svelte/transition";
+</script>
+
+<h3 class="message chatm" transition:fade><slot /></h3>
+
+<style lang="scss">
+  @import url("https://fonts.googleapis.com/css2?family=Urbanist:wght@600;900&display=swap");
+  h3 {
+    font-family: "Urbanist", sans-serif;
+    font-weight: 100;
+  }
+</style>
